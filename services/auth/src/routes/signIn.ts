e34dd56d1101a27a84a71fc270c6dffc18ca2d5b
Darlan H <darlanhermessjb@hotmail.com>
2022-05-23 20:35:03 -0300

    feat: signin user

--- services/auth/src/routes/signIn.ts
+++ services/auth/src/routes/signIn.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import BadRequestError from '../errors/badRequestError';
 import { validateRequest } from '../middlewares/validateRequest';
+import { User } from '../models/user';
+import { Password } from '../services/password';
 
 const signInRouter = Router();
 
@@ -9,8 +13,30 @@ const signInValidations = [
   body('password').trim().notEmpty().withMessage('You must supply a password'),
 ];
 
-signInRouter.post('/signin', ...signInValidations, validateRequest, (req, res) => {
-  return res.send('Hi signin');
+signInRouter.post('/signin', ...signInValidations, validateRequest, async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (!existingUser) {
+    throw new BadRequestError('Invalid login credentials');
+  }
+
+  const passwordsMatch = await Password.compare(existingUser.password, password);
+  if (!passwordsMatch) {
+    throw new BadRequestError('Invalid login credentials');
+  }
+
+  const userJwt = jwt.sign(
+    {
+      id: existingUser.id,
+      email: existingUser.email,
+    },
+    process.env.JWT_KEY as string,
+  );
+
+  req.session = { jwt: userJwt };
+
+  return res.json(existingUser);
 });
 
 export default signInRouter;
