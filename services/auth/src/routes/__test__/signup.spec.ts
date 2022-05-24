f7a546ba2602175124f5055ba055f825f8c73428
Darlan H <darlanhermessjb@hotmail.com>
2022-05-24 20:31:44 -0300

    test: unit tests for signup route

--- services/auth/src/routes/__test__/signup.spec.ts
+++ services/auth/src/routes/__test__/signup.spec.ts
@@ -2,11 +2,66 @@ import { StatusCodes } from 'http-status-codes';
 import request from 'supertest';
 import app from '../../app';
 
-it('returns a 201 successful signup', async () => {
-  const response = await request(app).post('/api/users/signup').send({
-    email: 'test@test.com',
-    password: 'password',
+describe('Sign up route', () => {
+  it('returns a 201 successful signup', async () => {
+    const response = await request(app).post('/api/users/signup').send({
+      email: 'test@test.com',
+      password: 'password',
+    });
+
+    expect(response.status).toBe(StatusCodes.OK);
   });
 
-  expect(response.status).toBe(StatusCodes.OK);
+  it('returns a 400 with an invalid email', async () => {
+    const response = await request(app).post('/api/users/signup').send({
+      email: 'invalid email',
+      password: 'password',
+    });
+
+    expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+  });
+
+  it('returns a 400 with an invalid password', async () => {
+    const response = await request(app).post('/api/users/signup').send({
+      email: 'test@test.com',
+      password: '1',
+    });
+
+    expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+  });
+
+  it('returns a 400 with missing email and password', async () => {
+    const response = await request(app).post('/api/users/signup').send({});
+
+    expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+  });
+
+  it('disallows duplicate emails', async () => {
+    await request(app)
+      .post('/api/users/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password',
+      })
+      .expect(200);
+
+    const response = await request(app).post('/api/users/signup').send({
+      email: 'test@test.com',
+      password: 'password',
+    });
+
+    expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+  });
+
+  it('sets a cookie after successful sign up', async () => {
+    const response = await request(app)
+      .post('/api/users/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password',
+      })
+      .expect(200);
+
+    expect(response.get('Set-Cookie')).toBeDefined();
+  });
 });
